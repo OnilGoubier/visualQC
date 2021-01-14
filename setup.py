9f13a4d3f439e347e479e5cc768020590b4861cb
Onil Goubier <ogoubier@warren-walter.com>
2021-01-14 14:08:27 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="visualQC", 
     version="0.0.1",
-    scripts=[ 
-        'visualQC/graphicGenerator.py', 
-        'visualQC/dirAndFiles.py',
-        'visualQC/plotStationsMap.py',
-        'visualQC/plotDataAvailability.py'],
+    #scripts=[ 
+    #    'visualQC/graphicGenerator.py', 
+    #    'visualQC/dirAndFiles.py',
+    #    'visualQC/plotStationsMap.py',
+    #    'visualQC/plotDataAvailability.py'],
     author="IPGP",   
     author_email="goubier@ipgp.fr",
     description="Scripts used to generate data plots",
@@ -22,7 +22,6 @@ setuptools.setup(
     package_data={"visualQC": ['visualQC/config/config.ini']},
     classifiers=[
         "Programming Language :: Python :: 3.7",
-        "License :: ?? :: ??",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
